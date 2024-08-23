8f45b946dbc86cb59232f6b87abfcac95e575c13
HyungJunYoo95 <u-jun11@nate.com>
2024-08-23 23:41:07 +0900

    style: 색상 수정

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -70,24 +70,13 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
           }
           className='flex items-center w-[120px]'
         >
-          <button className='flex items-center bg-teal-600 text-white px-4 py-2 rounded-full shadow-md hover:shadow-lg transition-shadow duration-300'>
+          <button className='flex items-center bg-gray-600 text-white px-4 py-2 rounded-full shadow-md hover:shadow-lg transition-shadow duration-300'>
             <FontAwesomeIcon icon={faUser} className='mr-2' />
             <span>{authenticate ? '로그아웃' : '로그인'}</span>
           </button>
         </div>
       </div>
 
-      {/* 모바일 메뉴 버튼 */}
-      <div className='bg-black sm:hidden'>
-        <button onClick={() => setMenuOpen(!menuOpen)}>
-          <FontAwesomeIcon
-            icon={menuOpen ? faTimes : faBars}
-            className='text-white p-4 text-3xl'
-          />
-        </button>
-      </div>
-
-      {/* 사이드바 */}
       {menuOpen && (
         <div className='fixed top-0 left-0 bg-white w-1/2 h-full z-10 shadow-lg flex flex-col'>
           <button className='self-end p-2' onClick={() => setMenuOpen(false)}>
@@ -99,7 +88,7 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
               className='text-lg font-semibold hover:text-teal-500 transition-colors duration-300 cursor-pointer p-2'
               onClick={() => {
                 handleMenu()
-                setMenuOpen(false) // 메뉴 닫기
+                setMenuOpen(false)
               }}
             >
               {item}
@@ -108,7 +97,15 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
         </div>
       )}
 
-      {/* 일반 네비게이션 */}
+      <div className='bg-black sm:hidden'>
+        <button onClick={() => setMenuOpen(!menuOpen)}>
+          <FontAwesomeIcon
+            icon={menuOpen ? faTimes : faBars}
+            className='text-white p-4 text-3xl'
+          />
+        </button>
+      </div>
+
       <nav className='hidden sm:flex sm:justify-center w-full p-4 shadow-md'>
         <div className='max-w-[1280px] grid grid-cols-2 gap-4 sm:flex sm:flex-wrap sm:justify-center'>
           {menuItems.map((item, index) => (
@@ -123,17 +120,17 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
         </div>
       </nav>
 
-      <div className='flex justify-center m-4 mr-[120px]'>
+      <div className='flex justify-center mt-4'>
         <input
           type='text'
-          placeholder='검색어를 입력하세요'
-          className='border border-gray-300 rounded-l-full px-4 py-3 w-96 text-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-teal-500 transition duration-200'
+          placeholder='상품명을 입력하세요'
+          className='border border-gray-300 rounded-l-full px-4 py-3 w-96 text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500 transition duration-200'
           value={searchKeyword}
           onChange={(e) => setSearchKeyword(e.target.value)}
           onKeyDown={handleKeyPress}
         />
-        <button className='flex items-center bg-teal-600 text-white px-6 py-3 rounded-r-full shadow-md hover:shadow-lg transition-shadow duration-300 hover:bg-teal-500 text-lg'>
-          <FontAwesomeIcon icon={faSearch} className='mr-2' />
+        <button className='flex items-center bg-gray-600 text-white px-6 py-3 rounded-r-full shadow-md hover:shadow-lg transition-shadow duration-300 hover:bg-gray-500 text-lg'>
+          <FontAwesomeIcon icon={faSearch} className='p-2' />
         </button>
       </div>
     </div>
